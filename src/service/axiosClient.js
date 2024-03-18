e7eadeab707ff06c228fd56f08adac226e93681c
huan572001 <85747713+huan572001@users.noreply.github.com>
2024-03-18 23:09:01 +0700

    add url

--- src/service/axiosClient.js
+++ src/service/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:8000/api/v1/",
+  baseURL: "https://server-laravel.onrender.com/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
